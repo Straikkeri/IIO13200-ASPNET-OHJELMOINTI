4f04db387d2e299f88a78e0bd26081566dde79b5
salesa <salesa@MEDI-426-WS-00>
2016-11-10 11:16:03 +0200

    EF CRUD-demo part 2/2 ready

--- Saitti/BookShopAP.aspx
+++ Saitti/BookShopAP.aspx
@@ -1,22 +1,52 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageAP.master" AutoEventWireup="true" CodeFile="BookShopAP.aspx.cs" Inherits="BookShopAP" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
-    <asp:Button ID="btnGetBooks" runat="server" 
-        Text="Hae Kirjat" OnClick="btnGetBooks_Click"/>
-    <asp:Button ID="btnGetCustomers" runat="server" 
-        Text="Hae Asiakkaat" OnClick="btnGetCustomers_Click"/>
-    <h1 style="w3-container">KirjakaupanX kirjat</h1>
-    <asp:GridView ID="gvBooks" runat="server" />
-    <h1 style="w3-container w3-blue">KirjakaupanX asiakkaat</h1>
-    <asp:GridView ID="gvCustomers" runat="server" />
-    <asp:DropDownList ID="ddlCustomers" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlCustomers_SelectedIndexChanged"></asp:DropDownList>
-    <h3>asiakkaan tilaukset</h3>
-    <asp:DropDownList ID="ddlOrders" runat="server" AutoPostBack="true" />
-    <!-- ilmoitukset käyttäjälle -->
-    <h2><asp:Label ID="lblMessages" runat="server" /></h2>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="Server">
+    <h1 style="w3-container">Esan KirjakaupanX kirjat</h1>
+    <div class="h3-row">
+        <!-- vasen lohko: asiakkaat -->
+        <div class="w3-container w3-third">
+            <asp:Button ID="btnGetCustomers" runat="server"
+                Text="Hae Asiakkaat" OnClick="btnGetCustomers_Click" />
+            <h2 style="w3-container w3-blue">KirjakaupanX asiakkaat</h2>
+            <asp:GridView ID="gvCustomers" runat="server" />
+
+        </div>
+        <!-- keskimmäinen lohko: asiakas CRUD -->
+        <div class="w3-container w3-third w3-light-gray">
+            <h3>Asiakkaan luonti ja muokkaus</h3>
+            <asp:DropDownList ID="ddlCustomers" runat="server"
+                AutoPostBack="True" OnSelectedIndexChanged="ddlCustomers_SelectedIndexChanged">
+            </asp:DropDownList>
+            <p>
+                etunimi:
+                <asp:TextBox ID="txtFirstname" runat="server" Text="..."></asp:TextBox>
+            </p>
+            <p>
+                sukunimi:
+                <asp:TextBox ID="txtLastname" runat="server" Text="..."></asp:TextBox>
+            </p>
+            <asp:Button ID="btnCreateCustomer" runat="server" Text="Luo uusi" CssClass="w3-btn" OnClick="btnCreateCustomer_Click" />
+            <asp:Button ID="btnSaveCustomer" runat="server" Text="Tallenna" CssClass="w3-btn" OnClick="btnSaveCustomer_Click" />
+            <asp:Button ID="btnDeleteCustomer" runat="server" Text="Poista" CssClass="w3-btn" OnClick="btnDeleteCustomer_Click" />
+
+        </div>
+        <!-- oikealla: kirjat-->
+        <div class="w3-container w3-third">
+            <asp:Button ID="btnGetBooks" runat="server"
+                Text="Hae Kirjat" OnClick="btnGetBooks_Click" />
+            <asp:GridView ID="gvBooks" runat="server" />
+        </div>
+    </div>
+    <div class="w3-row">
+        <h3>asiakkaan tilaukset</h3>
+        <asp:DropDownList ID="ddlOrders" runat="server" AutoPostBack="true" />
+        <!-- ilmoitukset käyttäjälle -->
+        <h2>
+            <asp:Label ID="lblMessages" runat="server" /></h2>
+    </div>
 </asp:Content>
-<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" runat="Server">
 </asp:Content>
 
--- Saitti/BookShopAP.aspx.cs
+++ Saitti/BookShopAP.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.UI.WebControls;
 public partial class BookShopAP : System.Web.UI.Page
 {
     protected static BookShopEntities ctx;
+    protected static bool KustiValittu;
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
@@ -41,6 +42,17 @@ public partial class BookShopAP : System.Web.UI.Page
         ddlCustomers.DataValueField = "id";
         ddlCustomers.DataBind();
         ddlCustomers.Items.Insert(0, string.Empty);//lisätään tyhjä rivi
+        //11.10
+        txtFirstname.Text = string.Empty;
+        txtLastname.Text = string.Empty;
+        SetButtons();
+    }
+    protected void SetButtons()
+    {
+        //TODO buttosten käytettävyys sen mukaan onko valittu kustia
+        btnCreateCustomer.Enabled = !KustiValittu;
+        btnSaveCustomer.Enabled = KustiValittu;
+        btnDeleteCustomer.Enabled = KustiValittu;
     }
     #endregion
     protected void btnGetBooks_Click(object sender, EventArgs e)
@@ -70,6 +82,11 @@ public partial class BookShopAP : System.Web.UI.Page
             {
                 lblMessages.Text 
                     = string.Format("Valitsit asiakkaan {0}", asiakas.lastname);
+                //jatko 11.10
+                txtFirstname.Text = asiakas.firstname;
+                txtLastname.Text = asiakas.lastname;
+                KustiValittu = true;
+                SetButtons();
                 //tutkitaan onko valitulla asiakkaalla tilauksia ja jos on näytetään
                 if (asiakas.Orders.Count > 0)
                 {
@@ -93,11 +110,100 @@ public partial class BookShopAP : System.Web.UI.Page
                                 string.Format("<br> -tilattu kirja {0} ", or.Book.name);
                         }
                     }
-                    
                 }
                 else
                 {
                     lblMessages.Text += ", ei ole tilauksia.";
+                }
+            }
+        }
+        else
+        {
+            lblMessages.Text = string.Empty;
+            KustiValittu = false;
+            txtFirstname.Text = string.Empty;
+            txtLastname.Text = string.Empty;
+            SetButtons();
+        }
+    }
+
+    protected void btnCreateCustomer_Click(object sender, EventArgs e)
+    {
+        //tarkistetaan ensin onko ko. asiakasta jo olemassa LINQ lambda-funktiolla
+        bool isThere = ctx.Customers.Any(c => (c.firstname.Contains(txtFirstname.Text) &
+        c.lastname.Contains(txtLastname.Text)));
+        if (isThere)
+        {
+            lblMessages.Text = string.Format("Asiakas {0} on jo olemassa", txtLastname.Text);
+        }
+        else
+        {
+            //luodaan kontekstiin uusi asiakas entiteetti
+            Customer kusti = new Customer();
+            kusti.firstname = txtFirstname.Text;
+            kusti.lastname = txtLastname.Text;
+            ctx.Customers.Add(kusti);
+            //save changes to db
+            ctx.SaveChanges();
+            lblMessages.Text = string.Format("uusi asiakas {0} {1} {2} luotu onnistuneesti",
+                kusti.id, kusti.firstname, kusti.lastname);
+            //päivitetään kontrollit
+            FillControls();
+        }
+
+    }
+
+    protected void btnSaveCustomer_Click(object sender, EventArgs e)
+    {
+        //haetaan valitun asiakkaan id
+        int i = int.Parse(ddlCustomers.SelectedValue);
+        if (i > 0)
+        {
+            var ret = ctx.Customers.Where(c => c.id == i);
+            Customer kusti = ret.FirstOrDefault();
+            if (kusti != null)
+            {
+                if (kusti.firstname != txtFirstname.Text)
+                    kusti.firstname = txtFirstname.Text;
+                if (kusti.lastname != txtLastname.Text)
+                    kusti.lastname = txtLastname.Text;
+                ctx.SaveChanges();
+                FillControls();
+            }
+        }
+    }
+
+    protected void btnDeleteCustomer_Click(object sender, EventArgs e)
+    {
+        //TODO poistetaan valittu
+        if (KustiValittu)
+        {
+            //poistetaan kontekstista ja db:stä
+            //huomioi myös businesslogiikka eli onko oikeast järkevää poistaa asiakas
+            //tässä casessa saa poistaa vain sellaisia asiakkaita joilla EI ole tilauksia
+            //haetaan valitun asiakkaan id
+            int i = int.Parse(ddlCustomers.SelectedValue);
+            if (i > 0)
+            {
+                var ret = ctx.Customers.Where(c => c.id == i);
+                Customer kusti = ret.FirstOrDefault();
+                if (kusti != null)
+                {
+                    //tutkitaan onko kustilla tilauksia, jos on ei poisteta
+                    if (kusti.Orders.Count == 0)
+                    {
+                        ctx.Customers.Remove(kusti);
+                        ctx.SaveChanges();
+                        //UIn päivitys
+                        KustiValittu = false;
+                        FillControls();
+                    }
+                    else
+                    {
+                        lblMessages.Text = string.Format
+                            ("asiakasta {0} ei voi poistaa, koska hänellä on {1} tilausta.",
+                            kusti.lastname, kusti.Orders.Count);
+                    }
                 }
             }
         }
