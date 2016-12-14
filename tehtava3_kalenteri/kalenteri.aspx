9b923766fcfb38fb20136e00ad2b959b88663426
Aleksi Tommila <aleksi.tommila@gmail.com>
2016-12-14 23:37:06 +0200

    tehty tehtävä 3, tehtävänannon mukainen ja toimiva

---
+++ tehtava3_kalenteri/kalenteri.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="kalenteri.aspx.cs" Inherits="kalenteri" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" ID="lblValitsePvm" Text="Valitse haluamasi päivä: " />
+            <asp:Label runat="server" ID="lblValittuPvm" Text="" /><br />
+            <asp:Label runat="server" Text="Valitun päivän ja tämän päivän erotus: " />
+            <asp:Label runat="server" ID="lblErotus" /><br />
+            <asp:Button runat="server" ID="btnTaakse" Text="<vuosi" OnClick="btnTaakse_Click" />
+            <asp:Button runat="server" ID="btnEteen" Text="vuosi>" OnClick="btnEteen_Click" />
+            <asp:Calendar runat="server" ID="kalenteri1" OnSelectionChanged="kalenteri1_SelectionChanged"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ tehtava3_kalenteri/kalenteri.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class kalenteri : System.Web.UI.Page
+{
+    DateTime valittuPvm;
+    DateTime nykyinenPvm;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        nykyinenPvm = kalenteri1.TodaysDate;
+        lblValittuPvm.Text = "valittu: " + valittuPvm.Day + "." + valittuPvm.Month + "." + valittuPvm.Year + " vuosi: " + valittuPvm.Year;
+    }
+
+    protected void btnTaakse_Click(object sender, EventArgs e)
+    {
+        if (kalenteri1.VisibleDate == DateTime.MinValue)
+        {
+            kalenteri1.VisibleDate = DateTime.Now;
+        }
+        kalenteri1.VisibleDate = kalenteri1.VisibleDate.AddYears(-1);
+    }
+
+    protected void btnEteen_Click(object sender, EventArgs e)
+    {
+        if (kalenteri1.VisibleDate == DateTime.MinValue)
+        {
+            kalenteri1.VisibleDate = DateTime.Now;
+        }
+
+        kalenteri1.VisibleDate = kalenteri1.VisibleDate.AddYears(1);
+    }
+
+    protected void kalenteri1_SelectionChanged(object sender, EventArgs e)
+    {
+        valittuPvm = kalenteri1.SelectedDate;
+        lblValittuPvm.Text = "valittu: " + valittuPvm.Day + "." + valittuPvm.Month + "." + valittuPvm.Year + " vuosi: " + valittuPvm.Year;
+        lblErotus.Text = (nykyinenPvm - valittuPvm).TotalDays.ToString();
+    }
+
+
+}
